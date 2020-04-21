4fc3db6648defb8a1909989483d5148812d22d5b
Hugh Denman <hdenman@users.noreply.github.com>
2020-04-21 20:04:13 +0100

    Combine.

--- c++/lazy-lambdas/example.cpp
+++ c++/lazy-lambdas/example.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class C {
+public:
+  int value;
+};
+
+void combine(const C& a, const C& b, C* out) {
+  out->value = a.value + b.value;
+}
+
 int main(int argc, char ** argv) {
-  cout << "It's OK.\n";
+  C source_1{2};
+  C source_2{4};
+  C dest;
+
+  combine(source_1, source_2, &dest);
+  cout << dest.value << endl;
+  
   return 0;
 }
