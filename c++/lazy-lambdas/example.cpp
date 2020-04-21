b05a9fad7f2c615b32831bde5a0d38d6a4344bd4
Hugh Denman <hdenman@users.noreply.github.com>
2020-04-21 20:51:37 +0100

    Combine: mix param opt

--- c++/lazy-lambdas/example.cpp
+++ c++/lazy-lambdas/example.cpp
@@ -7,6 +7,16 @@ public:
 };
 
 void combine(const C& a, const C& b, double mix, C* out) {
+  if (mix == 1.0) {
+    out->value = a.value;
+    return;
+  }
+
+  if (mix == 0.0) {
+    out->value = b.value;
+    return;
+  }
+
   out->value = (mix * a.value) + ((1.0 - mix) * b.value);
 }
 
@@ -15,7 +25,7 @@ int main(int argc, char ** argv) {
   C source_2{4};
   C dest;
 
-  combine(source_1, source_2, 0.5, &dest);
+  combine(source_1, source_2, 0.8, &dest);
   cout << dest.value << endl;
   
   return 0;
