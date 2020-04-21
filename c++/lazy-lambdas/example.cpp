5e08517766771299d91e9f887e5e85a11a2e560d
Hugh Denman <hdenman@users.noreply.github.com>
2020-04-21 20:06:04 +0100

    Combine: linear mix param

--- c++/lazy-lambdas/example.cpp
+++ c++/lazy-lambdas/example.cpp
@@ -6,8 +6,8 @@ public:
   int value;
 };
 
-void combine(const C& a, const C& b, C* out) {
-  out->value = a.value + b.value;
+void combine(const C& a, const C& b, double mix, C* out) {
+  out->value = (mix * a.value) + ((1.0 - mix) * b.value);
 }
 
 int main(int argc, char ** argv) {
@@ -15,7 +15,7 @@ int main(int argc, char ** argv) {
   C source_2{4};
   C dest;
 
-  combine(source_1, source_2, &dest);
+  combine(source_1, source_2, 0.5, &dest);
   cout << dest.value << endl;
   
   return 0;
