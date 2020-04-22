d9433effb08450d0d1b1a45f1a9617d558ecf4dc
Hugh Denman <hdenman@users.noreply.github.com>
2020-04-22 21:34:29 +0100

    C++ Lazy lambdas: add 'type_name'

--- c++/lazy-lambdas/example.cpp
+++ c++/lazy-lambdas/example.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <functional>
+#include <string_view>
 
 using namespace std;
+
+// https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c
+template <typename T>
+constexpr std::string_view type_name() {
+  std::string_view name, prefix, suffix;
+#ifdef __clang__
+  name = __PRETTY_FUNCTION__;
+  prefix = "std::string_view type_name() [T = ";
+  suffix = "]";
+#elif defined(__GNUC__)
+  name = __PRETTY_FUNCTION__;
+  prefix = "constexpr std::string_view type_name() [with T = ";
+  suffix = "; std::string_view = std::basic_string_view<char>]";
+#elif defined(_MSC_VER)
+  name = __FUNCSIG__;
+  prefix =
+      "class std::basic_string_view<char,struct std::char_traits<char> > "
+      "__cdecl type_name<";
+  suffix = ">(void)";
+#endif
+  name.remove_prefix(prefix.size());
+  name.remove_suffix(suffix.size());
+  return name;
+}
 
 class C {
 public:
@@ -29,6 +54,10 @@ void combine(getter get_a, getter get_b, double mix, C* out) {
 }
 
 void combine(const C& a, const C& b, double mix, C* out) {
+  auto lambda_1 = [&] {return a;};
+  auto lambda_2 = [&]() -> const C& {return a;};
+  cout << type_name<decltype(lambda_1)>() << endl;
+  cout << type_name<decltype(lambda_2)>() << endl;
   combine([&]() -> const C& {return a;},
 	  [&]() -> const C& {return b;},
 	  mix,
